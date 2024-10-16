04256dc7b25083600d4ec6be2582ae94dc96c19a
EduardoKroetz <eduardokroetz0@gmail.com>
2024-10-15 22:41:26 -0300

    perf: change ResultDto to PaginatedResultDto in GetMatchesUseCase

--- src/QuizDev.Application/UseCases/Matches/GetMatchesUseCase.cs
+++ src/QuizDev.Application/UseCases/Matches/GetMatchesUseCase.cs
@@ -2,6 +2,7 @@
 using QuizDev.Core.DTOs.Matches;
 using QuizDev.Core.DTOs.Quizzes;
 using QuizDev.Core.DTOs.Responses;
+using QuizDev.Core.DTOs.Results;
 using QuizDev.Core.Repositories;
 
 namespace QuizDev.Application.UseCases.Matches;
@@ -15,11 +16,11 @@ public class GetMatchesUseCase
         _matchRepository = matchRepository;
     }
 
-    public async Task<ResultDto> Execute(Guid userId, int pageSize, int pageNumber ,string? reference = null, string? status = null, bool? reviewed = null, string? orderBy = null)
+    public async Task<PaginatedResultDto> Execute(Guid userId, int pageSize, int pageNumber ,string? reference = null, string? status = null, bool? reviewed = null, string? orderBy = null)
     {
         var skip = pageSize * (pageNumber - 1);
         var matches = await _matchRepository.GetMatchesAsync(userId, skip, pageSize, reference, status, reviewed, orderBy);
 
-        return new ResultDto(matches);
+        return new PaginatedResultDto(pageSize, pageNumber, matches.Count, pageNumber + 1, matches);
     }
 }
